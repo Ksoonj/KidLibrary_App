a794f2eafc4bd4e3fb9aefe3c59b31b927715474
Ksoonj <kor3806@gmail.com>
2017-12-09 11:35:17 +0900

    Android

---
+++ SoonApp/kid_peace_library_App/app/src/main/java/comn/example/soonjae/kid_peace_library_app/adapter/CardContentAdapter.java
@@ -0,0 +1,37 @@
+package comn.example.soonjae.kid_peace_library_app.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import comn.example.soonjae.kid_peace_library_app.R;
+
+public class CardContentAdapter extends RecyclerView.Adapter<CardContentAdapter.ViewHolder> {
+
+    // Set numbers of List in RecyclerView.
+    private static final int LENGTH = 18;
+
+    public CardContentAdapter() {
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // no-op
+    }
+
+    @Override
+    public int getItemCount() {
+        return LENGTH;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_card, parent, false));
+        }
+    }
+}
